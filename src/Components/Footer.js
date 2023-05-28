1909d8b0ad2e814bfcf5b2dba1a5bf6a88de613c
joselinecedano <joselinecedano0@gmail.com>
2023-05-28 15:59:52 -0400

    nav and footer set up

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='footer'>
+        <Link className = 'footer-link' to = 'https://instagram.com/kissmylashesz?igshid=MzRlODBiNWFlZA=='> <div> Insta </div></Link>
+        <Link className = 'footer-link' to = 'https://www.facebook.com/kissmylasheszz?mibextid=LQQJ4d'> <div> Facebook </div></Link>
+        <Link className = 'footer-link' to = 'https://www.tiktok.com/@kissmylashesz?_t=8chb7xPO69u&_r=1'> <div> Tiktok </div></Link>
+    </footer>
   )
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <nav className='nav'>
+        <Link className='nav-link' to = '/about'><div> About </div></Link>
+        <Link className='nav-link' to = '/services'><div> Services </div></Link>
+        <Link className='nav-link' to = '/training'><div> Training </div></Link>
+        <Link className='nav-link' to = '/products'><div> Products </div></Link>
+        <Link className='nav-link' to = '/gallery'><div> Gallery </div></Link>
+        <Link className='nav-link' to = '/user'><div> Account </div></Link>
+    </nav>
   )
 }
 export default Header
