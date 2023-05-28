33f465cc679c3675cd621c2a22bb90c008d98d69
joselinecedano <joselinecedano0@gmail.com>
2023-05-27 20:40:55 -0400

    basic set up done, imported router

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './Components/Header.js'
+import Footer from './Components/Footer.js'
+import Main from './Components/Main.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
