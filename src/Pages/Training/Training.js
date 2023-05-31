48d50172024109289a299263e585cc30647500f0
joselinecedano <joselinecedano0@gmail.com>
2023-05-31 19:54:22 -0400

    rendering training courses index shell, working on set up now

---
+++ src/Pages/Training/Training.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import TrainingIdx from './indexT';
+
+const Training = (props) => {
+    const [courses, setCourse] = useState(null)
+
+    const trainingUrl = process.env.REACT_APP_TRAINING_COURSES_URL
+
+    const getCourses = async () => {
+        const response = await fetch (trainingUrl)
+        const data = await response.json()
+        setCourse(data.data)
+    }
+
+    useEffect(()=>{
+        getCourses()
+    }, [])
+
+  return (
+    <section>
+        <Routes>
+            <Route path='/' element={<TrainingIdx />}/>
+        </Routes>
+    </section>
+  )
+}
+
+export default Training
---
+++ src/Pages/Training/indexT.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const TrainingIdx = (props) => {
+  
+  return (
+    <div>TrainingIdx</div>
+  )
+}
+
+export default TrainingIdx
