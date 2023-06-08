50d53267ded88fe197775b29b89a5018a050466b
joselinecedano <joselinecedano0@gmail.com>
2023-06-07 21:02:55 -0400

    working nav and about set up

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,8 +5,10 @@ const Footer = () => {
   return (
     <>
      <footer  className='flex justify-center '>
+        <Link  to = 'https://www.facebook.com/kissmylasheszz?mibextid=LQQJ4d'> <div> <img className='h-10'src='/images/facebook.png' alt='facebook'/></div></Link> 
+
         <Link to = 'https://instagram.com/kissmylashesz?igshid=MzRlODBiNWFlZA=='> <div> <img className='h-10 ' src='/images/instagram.png' alt='insta'/> </div></Link>
-        <Link  to = 'https://www.facebook.com/kissmylasheszz?mibextid=LQQJ4d'> <div> <img className='h-10'src='/images/facebook.png' alt='facebook'/></div></Link>
+
         <Link to = 'https://www.tiktok.com/@kissmylashesz?_t=8chb7xPO69u&_r=1'> <div> <img className='h-10' src='/images/tiktok.png' alt='tiktok'/> </div></Link>
     </footer>
     <div className='text-center'> 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,33 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-export const Header = () => {
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <nav>
-      <Link to="/">
-        <img
-          className="logo bg-transparent"
-          src="/images/cherry.png"
-          alt="cherry"
-        />
-      </Link>
+    <header>
+      <nav className="flex flex-wrap items-center justify-between w-full py-4 md:py-0 px-4 text-lg">
+        <div className="flex items-center">
+          <Link to="/">
+            <img className="logo" src="/images/cherry.png" alt="cherry" />
+          </Link>
+           <a className=" ml-4 italic drop-shadow-glow"  href="https://app.acuityscheduling.com/schedule.php?owner=19702883">Book Now</a>
+        </div>
+       
 
-      <Link to="/services">
-        <div> Services </div>
-      </Link>
-
-      <Link to="/training">
-        <div> Training </div>
-      </Link>
-
-      <Link to="/products">
-        <div> Products </div>
-      </Link>
-
-      <Link to="/gallery">
-        <div> Gallery </div>
-      </Link>
-    </nav>
+        <div>
+          <img
+            id="menu-button"
+            className="h-6 w-6 cursor-pointer md:hidden block"
+            src="/images/circle-menu.png"
+            alt="menu"
+            onClick={toggleMenu}
+          />
+        </div>
+        <div
+          className={`${
+            menuOpen ? "" : "hidden"
+          } w-full md:flex md:items-center md:w-auto`}
+          id="menu-button"
+        >
+          <ul className="pt-4 text-end md:flex md:justify-between md:pt-0">
+            <li>
+              <Link
+                className="md:p-4 py-2 block hover:text-red-200"
+                to="/services"
+                onClick={toggleMenu}
+              >
+                <div> Services </div>
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="md:p-4 py-2 block hover:text-red-200"
+                to="/training"
+                onClick={toggleMenu}
+              >
+                <div> Training </div>
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="md:p-4 py-2 block hover:text-red-200"
+                to="/products"
+                onClick={toggleMenu}
+              >
+                <div> Products </div>
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="md:p-4 py-2 block hover:text-red-200"
+                to="/gallery"
+                onClick={toggleMenu}
+              >
+                <div> Gallery </div>
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="md:p-4 py-2 block hover:text-red-200"
+                to="/about"
+                onClick={toggleMenu}
+              >
+                <div> Our Story </div>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
   );
 };
 export default Header;
