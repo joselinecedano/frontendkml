315e70328bd4f339bf64de7a513e0c6390946dff
joselinecedano <joselinecedano0@gmail.com>
2023-06-08 20:51:39 -0400

    main styling for services index is done, added custom pulse effect! need to make a seperate schema for fill ins so theyre under fill in section of services

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const KissMyLashesz = () => {
   return (
       <section className="h-screen flex items-center justify-center">
-        <div className="text-7xl font-oldlondon drop-shadow-glow text-center md:text-9xl lg:text-10xl">
+        <div className="text-7xl font-oldlondon drop-shadow-glow text-center md:text-9xl lg:text-10xl animate-pulse">
           kissmylashesz
         </div>
       </section>
--- src/Pages/Services/indexS.js
+++ src/Pages/Services/indexS.js
@@ -5,8 +5,38 @@ const ServicesIdx = (props) => {
   //loaded function
   const loaded = () => {
     return (
-      <>
-        <div className="flex flex-wrap items-center justify-center py-16 ">
+      <section className="min-h-screen "> 
+      <h1 className="font-oldlondon text-6xl text-center"> Services</h1><br/>
+        <div className="flex flex-wrap justify-center border rounded-xl border-x-amber-950">
+          <div className="w-full sm:w-1/2 p-2 ">
+            <div className="text-center">
+              <h1 className="font-oldlondon text-xl "> Policies </h1>
+              <p>
+                § Our regulare hours are from 8am - 5pm. Any appointments booked
+                outside of our scheduled hours will be considered a sqeeze in
+                appointment, which comes with a fee of $15.
+                <br />§ We have a 10 minute grace period! Anything after is a{" "}
+                <span className="underline text-red-500">cancellation</span>.
+              </p>
+            </div>
+          </div>
+          <div className="w-full sm:w-1/2 p-2">
+            <div className="text-center">
+              <h1 className="font-oldlondon text-xl"> Deposits </h1>
+              <p>
+                § A non-refundable $25 deposit is required to book appointment.
+                <br />
+                § A $1 electronic fee is included. § $25 deposits are deducted
+                off the final price. <br /> § Deposits can be transferred when
+                rescheduling appointments if a 24hr notice has been given.{" "}
+                <br /> § Appointments are not booked until a deposit and
+                confirmation email has been received.
+              </p>
+            </div>
+          </div>
+        </div><br/>
+       
+        <div className="flex flex-wrap items-center justify-center ">
           {props.services.map((service) => (
             <div
               key={service._id}
@@ -26,8 +56,23 @@ const ServicesIdx = (props) => {
             </div>
           ))}
         </div>
+        <br/>
+        <div className="border rounded-xl border-x-amber-950">
+        <div className="w-full sm:w-full p-2 ">
+            <div className="text-center">
+              <h1 className="font-oldlondon text-xl"> Fill Ins </h1>
+              <p>
+              § You must have at least 50% of your lashes left,
+otherwise you'll be charged a full set.<br/> § Fill is need to be booked within a 2-3 week period, anything after will be considered a full set. <br/> § After 4 weeks a majority of remaining lashes will be extremely outgrown and need a removal. If this hasn't been booked in prior, I may not be able to complete a
+fullset within your allocated slot. <br/>§ I don't infill other lash tech's work. You would need
+to book in for a removal and a fresh set.
+              </p>
+            </div>
+          </div>
+        </div>
         <Link to="/services/form">Add a Service</Link>
-      </>
+        
+      </section>
     );
   };
 
