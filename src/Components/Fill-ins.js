539dfe8f2a014ec4f214414f1a7a83ad261a8285
joselinecedano <joselinecedano0@gmail.com>
2023-06-09 14:23:14 -0400

    added book button to services

---
+++ src/Components/Fill-ins.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const FillIns = () => {
+  return (
+    <div>
+    <div className="border rounded-xl border-x-amber-950">
+        <div className="w-full sm:w-full p-2 ">
+            <div className="text-center">
+              <h1 className="font-oldlondon text-xl"> Fill Ins </h1>
+              <p>
+              § You must have at least 50% of your lashes left,
+otherwise you'll be charged a full set.<br/> § Fill is need to be booked within a 2-3 week period, anything after will be considered a full set. <br/> § After 4 weeks a majority of remaining lashes will be extremely outgrown and need a removal. If this hasn't been booked in prior, I may not be able to complete a
+fullset within your allocated slot. <br/>§ I don't infill other lash tech's work. You would need
+to book in for a removal and a fresh set.
+              </p>
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default FillIns
--- src/Pages/Services/indexS.js
+++ src/Pages/Services/indexS.js
@@ -12,7 +12,7 @@ const ServicesIdx = (props) => {
           {props.services.map((service) => (
             <div
               key={service._id}
-              className="relative p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5"
+              className="relative p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 flex flex-col items-center"
             >
               <Link to={`/services/${service._id}`}>
                 <img
@@ -25,23 +25,11 @@ const ServicesIdx = (props) => {
                 {" "}
                 {service.name}
               </h1>
+              <button className="border rounded-xl border-x-amber-950 p-1 mt-auto"><a href="https://app.acuityscheduling.com/schedule.php?owner=19702883">Book Now</a></button>
             </div>
           ))}
         </div>
         <br/>
-        <div className="border rounded-xl border-x-amber-950">
-        <div className="w-full sm:w-full p-2 ">
-            <div className="text-center">
-              <h1 className="font-oldlondon text-xl"> Fill Ins </h1>
-              <p>
-              § You must have at least 50% of your lashes left,
-otherwise you'll be charged a full set.<br/> § Fill is need to be booked within a 2-3 week period, anything after will be considered a full set. <br/> § After 4 weeks a majority of remaining lashes will be extremely outgrown and need a removal. If this hasn't been booked in prior, I may not be able to complete a
-fullset within your allocated slot. <br/>§ I don't infill other lash tech's work. You would need
-to book in for a removal and a fresh set.
-              </p>
-            </div>
-          </div>
-        </div>
         <Link to="/services/form">Add a Service</Link>
         
       </div>
