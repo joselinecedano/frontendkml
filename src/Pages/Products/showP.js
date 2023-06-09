6350ed16533fc557d298c0b434ec5528da1df96d
joselinecedano <joselinecedano0@gmail.com>
2023-06-09 13:09:33 -0400

    styling for products show

--- src/Pages/Products/showP.js
+++ src/Pages/Products/showP.js
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { Link, useNavigate } from "react-router-dom";
 
 const ProductShow = (props) => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const params = useParams();
   const id = params.id;
   const products = props.products;
@@ -11,18 +11,29 @@ const ProductShow = (props) => {
 
   //linking edit button to edit route
   const edit = (e) => {
-    navigate(`/products/edit/${product._id}`)
-  }
+    navigate(`/products/edit/${product._id}`);
+  };
   return (
-    <div>
-      <img src={product.image} alt={product.name} />
-      <h2> {product.name}</h2>
-      <h3> ${product.price}</h3>
-      <h4> {product.description}</h4>
+    <div className="min-h-screen">
+      <div className="flex flex-col sm:flex-row border rounded-xl border-x-amber-950 p-2">
+        <img
+          className="object-scale-down h-80 rounded-lg sm:w-1/2 sm:order-first"
+          src={product.image}
+          alt={product.name}
+        />
+        <div className="ml-4">
+          <h2 className="font-oldlondon text-4xl text-center mt-4">
+            {" "}
+            {product.name}
+          </h2>
+          <h3 className="text-center text-xl italic"> ${product.price}</h3>
+          <h4 className="text-lg text-center"> {product.description}</h4>
+        </div>
+      </div>
       <div>
         <button onClick={edit}> Edit </button>
       </div>
-      <Link to="/products"> Back to products</Link>
+      <Link to="/products"> Back to Products</Link>
     </div>
   );
 };
