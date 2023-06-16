5cc9fd31049f84f4f68e6d59a3954fbdcd856e90
joselinecedano <joselinecedano0@gmail.com>
2023-06-15 20:42:26 -0400

    deployed frontend

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -4,7 +4,7 @@ const KissMyLashesz = () => {
   return (
       <section className="h-screen flex items-center justify-center">
         <div className="text-7xl font-oldlondon drop-shadow-glow text-center md:text-9xl lg:text-10xl animate-pulse">
-          kissmylashesz 
+          kissmylashesz
         </div>
       </section>
   );
